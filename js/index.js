640f6a06d0fa989d7f45d7e053f8baaf4b5aeab5
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-11-07 15:25:24 +0100

    made it work

--- js/index.js
+++ js/index.js
@@ -1,13 +1,61 @@
+const baseUri = "http://jsonplaceholder.typicode.com/todos"
+
 const app = Vue.createApp({
     data() {
         return {
-            // add data here
+            posts: [],
+            error: null,
+            userId: ""
         }
         
     },
 
     methods: {
         // add methods here
+        getAllPosts() {
+            axios.get(baseUri)
+            .then(response => {
+                console.log("in function getAllPosts")
+                console.log("status code: "+ response.status)
+
+                this.posts = response.data
+
+                console.log("length of the posts in array: " + this.posts.length)
+            })
+            .catch(error = (ex) => {
+                this.posts = []
+                this.error = ex.message
+                console.log("Error:" + this.error)
+            })
+        },
+
+        clearList() {
+            this.posts = []
+            this.error = null
+            console.log("count posts: " + this.posts.length)
+        },
+
+        getByUserId(id) {
+            axios.get(baseUri, {
+                params: {
+                    userId: id
+                }
+            })
+            .then(response => {
+                console.log("in function getByUserId")
+                console.log("status code: " + response.status)
+
+                this.posts = response.data
+
+                console.log("length of the posts in array: " + this.posts.length)
+
+            })
+            .catch(error = (ex) => {
+                this.posts = []
+                this.error = ex.message
+                console.log("Error:" + this.error)
+            })
+        }
     }
     
 })
